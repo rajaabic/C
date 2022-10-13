f359058e67c62f9a1651ab32a23212a1292117bc
rajaabic <114388508+rajaabic@users.noreply.github.com>
2022-10-13 17:30:15 +0530

    Create input.c

---
+++ input.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(_)
+{
+    float number1=13.5;
+    double number2=12.4;
+
+    printf("number1= %f\n",number1);
+    printf("number2= %lf",number2);
+    return 0;
+}
+#include<stdio.h>
+int main()
+{ 
+    int testInteger=5;
+    printf("number=%d", testInteger);
+    return 0;
+}
+#include<stdio.h>
+int main()
+{
+    char chr='a';
+    printf("character=%c",chr);
+    return 0;
+}
+

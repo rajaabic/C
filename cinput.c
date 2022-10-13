f5e72fdd5a3c9c47abb412195415afc4d2946ecb
rajaabic <114388508+rajaabic@users.noreply.github.com>
2022-10-13 17:26:03 +0530

    Create cinput.c

---
+++ cinput.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float num1;
+    double num2;
+    printf("enter a number:");
+    scanf("%f",&num1);
+    printf("enter another number:");
+    scanf("%lf",num2);
+    return 0;
+}

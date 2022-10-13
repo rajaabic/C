92327d80d68c063bf55bb5c65665c6bea1870649
rajaabic <114388508+rajaabic@users.noreply.github.com>
2022-10-13 17:38:06 +0530

    Create multiplevalues4.c

---
+++ multiplevalues4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float num1;
+    double num2;
+    printf("enter a number:");
+    scanf("%f",&num1);
+    printf("enter another number:");
+    scanf("%lf",&num2);
+    printf("num1=%f\n",num1);
+    printf("num2=%lf",num2);
+    return 0;
+
+}

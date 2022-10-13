6ac6c001dcd49a39763f67508ab2d8c860615670
rajaabic <114388508+rajaabic@users.noreply.github.com>
2022-10-13 17:34:49 +0530

    Create multiple values1.c

---
+++ multiple values1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+   int a;
+   printf("Enter an integer\n");
+   scanf("%d",&a);
+   print("Integer that you have entered is %d\n",a);
+   return 0;
+}
